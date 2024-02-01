2f8844795c580693b748c8060da0a1acc436a380
developermong2 <developermong2@gmail.com>
2024-02-01 14:30:10 +0900

    Feat : 컴포넌트 Editor.tsx로 분리

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
 
 interface Todo {
   id: number;
@@ -8,17 +9,13 @@ interface Todo {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [text, setText] = useState("");
 
   // 리액트에서는 이렇게 배열 형태로 저장된 요소들을 리스트 렌더링을 할 때 모든
   // 요소가 중복되지 않는 고유한 키를 가져야 한다.
   // id를 요소를 추가할 때마다 증가시켜주자.
   const idRef = useRef(0);
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
-  };
-  const onClickAdd = () => {
+  const onClickAdd = (text: string) => {
     setTodos([
       ...todos,
       {
@@ -26,7 +23,6 @@ function App() {
         content: text,
       },
     ]);
-    setText("");
   };
 
   useEffect(() => {
@@ -36,8 +32,7 @@ function App() {
   return (
     <div className="App">
       <h1>Todo</h1>
-      <input value={text} onChange={onChangeInput} />
-      <button onClick={onClickAdd}>추가</button>
+      <Editor onClickAdd={onClickAdd}></Editor>
     </div>
   );
 }
